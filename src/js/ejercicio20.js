39a8a963bb93e05df6f06cc8481bdf3254f8244c
FrancoPonce579 <poncefranco957@gmail.com>
2026-04-30 17:42:11 -0300

    Asistencia jueves 30/04 FrancoPonce

--- src/js/ejercicio20.js
+++ src/js/ejercicio20.js
@@ -1,2 +1,3 @@
 import { servicioFacundoMatorras } from "../services/serviceFacundoMatorras";
 import { servicioAlejandroFernandez } from "../services/ServiceAlejandroFernandez";
+import { servicioFrancoPonce} from "../services/serviceFrancoPonce";
